13e977600d98dcf79aa899ad1ec752b071e113e9
Torsten Compart <torsten.compart@ionos.com>
2022-10-19 09:20:03 +0200

    First end2end test to run app and test against it

---
+++ app.test.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+describe('Restaurant', () => {
+  test('says Hello World', async () => {
+    await axios.get('http://127.0.0.1:3000')
+      .catch(e => {
+        console.log('An error was thrown: ', e.code);
+      })
+      .then(r => {
+        console.log("Response: ", r);
+        expect(r.status).toBe(200);
+        expect(r.data).toBe('Hello World');
+      })
+    ;
+  });
+});
