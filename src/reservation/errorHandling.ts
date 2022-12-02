a5bc8bad12142b5adb26d5d7c480b73b6c167a70
Torsten Compart <torsten.compart@ionos.com>
2022-12-02 08:39:11 +0100

    Reduce complexity by extracting error handling

---
+++ src/reservation/errorHandling.ts
@@ -0,0 +1,21 @@
+import {Response} from 'express';
+import {BadRequest} from "./reservation";
+
+export class ErrorHandling {
+    private _resp: Response;
+
+    constructor(resp: Response) {
+        this._resp = resp;
+    }
+
+
+    onrejected = (e: Error) => {
+        if (e instanceof BadRequest) {
+            console.log("BadRequest was catched: ", e);
+            this._resp.status(400);
+            this._resp.send('Bad request.');
+        } else {
+            this._resp.status(500);
+        }
+    };
+}
--- src/reservation/routes.ts
+++ src/reservation/routes.ts
@@ -1,23 +1,18 @@
 import {Request, Response} from 'express';
 import {getReservationRepository} from "./service-injection";
-import {BadRequest, ReservationController} from "./reservation";
+import {ReservationController} from "./reservation";
+import {ErrorHandling} from "./errorHandling";
 
 export function createReservationRoute(reservationRepository = getReservationRepository) {
     return (req: Request, res: Response) => {
-        let jsonContent = JSON.stringify({
-            "msg": "Reservation accepted."
-        });
+        const onfulfilled = () => {
+            res.setHeader('Content-Type', 'application/json;charset=utf-8');
+            res.status(201);
+            res.send(JSON.stringify({
+                "msg": "Reservation accepted."
+            }))
+        };
         new ReservationController(reservationRepository()).post(req.body)
-            .then(() => {
-                res.setHeader('Content-Type', 'application/json;charset=utf-8');
-                res.status(201);
-                res.send(jsonContent)
-            }).catch(e => {
-            if (e instanceof BadRequest) {
-                console.log("BadRequest was catched: ", e);
-                res.status(400)
-                res.send('Bad request.')
-            }
-        })
+            .then(onfulfilled).catch(new ErrorHandling(res).onrejected)
     };
 }
