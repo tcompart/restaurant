d35d2bd409b31628b2d0c48916c00c3602bf71ef
Torsten Compart <torsten.compart@365farmnet.com>
2024-01-23 20:04:03 +0100

    Seeing full coverage finally

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: 'node',
+  collectCoverageFrom: ["src/**/*.ts", "src/**/*.js", "!src/**/*.test.*"],
   testResultsProcessor: "jest-junit",
   coverageReporters: ["json"],
   setupFiles: ["dotenv/config"],
