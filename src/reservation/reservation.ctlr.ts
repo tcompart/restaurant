aee55c864421ec05a54322eadeb1414085e0e96a
Torsten Compart <torsten.compart@365farmnet.com>
2024-01-25 13:05:57 +0100

    Found solution for promise that returns. Using immediately invoked function expression as well

--- src/reservation/reservation.ctlr.ts
+++ src/reservation/reservation.ctlr.ts
@@ -3,30 +3,38 @@ import {Identifiable, ReservationImpl} from "./reservation.impl";
 import {BadRequest, ReservationRepository} from "./reservation";
 
 export class ReservationController {
-    private _repository: ReservationRepository;
+  private _repository: ReservationRepository;
 
-    constructor(db: ReservationRepository) {
-        this._repository = db;
-    }
+  constructor(db: ReservationRepository) {
+    this._repository = db;
+  }
 
-    async post(reservationDTO: ReservationDTO): Promise<Identifiable> {
-        return new Promise<Identifiable>((resolve, reject) => {
-            const reservation = new ReservationImpl(reservationDTO.at, reservationDTO.email, reservationDTO.name, reservationDTO.quantity);
-            return this._repository.findReservationsOnDate(reservation.at)
-                .then(reservationsOnDate => {
-                    if (Array.isArray(reservationsOnDate)) {
-                        const amount = reservationsOnDate
-                            .map(r => r.quantity)
-                            .reduce((a, b) => a + b, 0);
-                        if ((amount + reservationDTO.quantity) >= 10) {
-                            reject(new BadRequest("Too many reservations.", "409"));
-                        }
-                        return amount;
-                    }
-                    return 0;
-                })
-                .then(() => resolve(this._repository.create(reservation)));
-        })
-            .catch((reason) => Promise.reject(new BadRequest(reason.message, "400")));
-    }
+  post(reservationDTO: ReservationDTO): Promise<Identifiable> {
+    return new Promise<Identifiable>((resolve, reject) => {
+      (async () => {
+        let reservation = null;
+        try {
+          reservation = new ReservationImpl(reservationDTO.at, reservationDTO.email, reservationDTO.name, reservationDTO.quantity);
+        } catch (error: any) {
+          reject(new BadRequest(error.message, "400"));
+        }
+        if (reservation && reservation.at) {
+          try {
+            const reservations = await this._repository.findReservationsOnDate(reservation.at);
+            if (Array.isArray(reservations)) {
+              const amount = reservations
+              .map(r => r.quantity)
+              .reduce((a, b) => a + b, 0);
+              if ((amount + reservationDTO.quantity) >= 10) {
+                reject(new BadRequest("Too many reservations.", "400"));
+              }
+            }
+            resolve(this._repository.create(reservation));
+          } catch (reason: any) {
+            reject(new BadRequest(reason.message, reason.name));
+          }
+        }
+      })();
+    });
+  }
 }
